928c459f57a366474ab3fa35e6751d12a4b97307
tragisch <uwe@roettgermann.de>
2023-11-22 22:31:31 +0100

    simplify example

--- examples/bench_function.c
+++ examples/bench_function.c
@@ -9,21 +9,14 @@
 #include "simple_bench.h"
 
 void sleep_some_time(int seconds, int memory_usage) {
-  sleep(seconds);
-  // allocate 2MB of memory
-
   const int MEGABYTE = 1024 * 1024;
   const int MAX_RANDOM_VALUE = 255;
   char *store_some_memory = malloc((unsigned long)memory_usage * MEGABYTE);
-  // fill the memory with random data
-  for (int i = 0; i < memory_usage * MEGABYTE; i++) {
-    store_some_memory[i] = (char)(arc4random() % MAX_RANDOM_VALUE);
-  }
-  // free the memory
+  sleep(seconds);
   free(store_some_memory);
 }
 
 int main() {
-  BENCH(sleep_some_time(3, 4));
+  BENCH(sleep_some_time(3, 2000));
   return 0;
 }
